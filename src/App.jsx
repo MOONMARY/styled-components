b8174bb5f4e231a405bebf8acd02bb61709610a4
MOONMARY <firstmoon5@naver.com>
2024-12-20 15:14:26 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import StyledComponents from "./styled/StyledComponents";
 import PropsComponents from "./styled/PropsComponents";
 import StyledDiv from "./styled/UserPropsComponents";
-
 function App() {
   return (
     <div>
       <h1>StyledComponents</h1>
       <StyledComponents />
       <h1>PropsComponents</h1>
-      <PropsComponents />
+      {/* <PropsComponents /> */}
       <h1>UserPropsComponents</h1>
-      <styledDiv dark={false} />
-      <styledDiv dark={true} />
+      <StyledDiv dark={false} />
+      <StyledDiv dark={true} />
     </div>
   );
 }
